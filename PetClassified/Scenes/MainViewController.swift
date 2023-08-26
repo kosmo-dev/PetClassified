3c9faf6d6ad36f544cc12343cf198e92a4a695e7
Vadim Kuzmin <kuzminvadima@yandex.ru>
2023-08-26 07:53:55 +0300

    Removed strotyboard

--- PetClassified/Scenes/MainViewController.swift
+++ PetClassified/Scenes/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  PetClassified
 //
 //  Created by Вадим Кузьмин on 24.08.2023.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
